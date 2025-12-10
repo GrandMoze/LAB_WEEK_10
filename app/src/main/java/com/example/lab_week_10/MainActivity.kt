b2c86ae2793657e4bfd9d5a337cbdd01081ad253
GrandMoze <grandmozee@gmail.com>
2025-12-10 13:13:05 +0700

    Commit No. 1: Implement ViewModel to Handle Rotation

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.lab_week_10
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textTotal: TextView
+    private lateinit var buttonPressMe: Button
+
+    // ViewModel supaya total nggak reset saat rotate
+    private val viewModel: TotalViewModel by lazy {
+        ViewModelProvider(this)[TotalViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        textTotal = findViewById(R.id.text_total)
+        buttonPressMe = findViewById(R.id.button_press_me)
+
+        // set tampilan awal dari ViewModel
+        updateTotalText(viewModel.total)
+
+        buttonPressMe.setOnClickListener {
+            val newTotal = viewModel.incrementTotal()
+            updateTotalText(newTotal)
         }
+    }
+
+    private fun updateTotalText(value: Int) {
+        textTotal.text = "Total: $value"
     }
 }
